6fee610619a56653d1a6167e4a0454ceb9f72cba
SergiuPopistasItSchool <sergiu.popistas@itschool.ro>
2022-04-13 19:54:21 +0300

    made a cout whit anoter another message

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Another Hello\n";
+    std::cout << "Another Another commit\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
